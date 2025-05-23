b9d8fa3253ad76969990333ef0879a8978edd311
Swati <Swatigupta2225@gmail.com>
2025-05-23 18:04:19 +0530

    Fix: align ReactDOM typings with React 17

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import NotesPage from './pages/NotesPage';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <NotesPage />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-serviceWorkerRegistration.register();
